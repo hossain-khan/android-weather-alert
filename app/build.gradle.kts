f316074db6b838c4de856ed16c22ae4ede1703ab
Hossain Khan <hello@hossain.dev>
2024-12-24 18:09:53 -0500

    [ADDED] [Gemini] Dependencies needed for later.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,21 @@ dependencies {
     implementation(libs.anvil.annotations)
     implementation(libs.anvil.annotations.optional)
 
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+
+    // WorkManager
+    implementation(libs.androidx.work.runtime.ktx)
+
+    // Data Store
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore.core)
+
+    // Glance
+    implementation(libs.androidx.glance)
+    implementation(libs.androidx.glance.appwidget)
+
     // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
