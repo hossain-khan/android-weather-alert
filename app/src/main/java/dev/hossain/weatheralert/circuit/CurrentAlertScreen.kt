ec60c68cb94c9d075de2eddb33317397a4bb683d
Hossain Khan <hello@hossain.dev>
2024-12-29 15:39:32 -0500

    [ADDED] Remove item snippet from example - WIP

--- app/src/main/java/dev/hossain/weatheralert/circuit/CurrentAlertScreen.kt
+++ app/src/main/java/dev/hossain/weatheralert/circuit/CurrentAlertScreen.kt
@@ -1,23 +1,32 @@
 package dev.hossain.weatheralert.circuit
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,13 +34,17 @@ import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,6 +52,7 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.CircuitUiEvent
@@ -161,6 +175,9 @@ fun CurrentWeatherAlerts(
     modifier: Modifier = Modifier,
 ) {
     WeatherAlertAppTheme {
+        val scope = rememberCoroutineScope()
+        val snackbarHostState = remember { SnackbarHostState() }
+
         Scaffold(
             topBar = {
                 TopAppBar(title = { Text("Weather Alerts") })
@@ -174,6 +191,7 @@ fun CurrentWeatherAlerts(
                     icon = { Icon(Icons.Default.Add, contentDescription = "Add Alert") },
                 )
             },
+            snackbarHost = { SnackbarHost(snackbarHostState) },
             content = { padding ->
                 Column(
                     modifier =
@@ -181,22 +199,111 @@ fun CurrentWeatherAlerts(
                             .fillMaxSize()
                             .padding(padding),
                 ) {
-                    AlertTileGrid(tiles = state.tiles)
+                    AlertTileGrid(tiles = state.tiles) {}
                 }
             },
         )
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun AlertTileGrid(tiles: List<AlertTileData>) {
-    LazyVerticalGrid(
-        columns = GridCells.Adaptive(minSize = 150.dp),
-        contentPadding = PaddingValues(8.dp),
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        items(tiles) { tile ->
-            AlertTile(data = tile, modifier = Modifier.fillMaxWidth())
+fun AlertTileGrid(
+    tiles: List<AlertTileData>,
+    onUndo: (AlertTileData) -> Unit,
+) {
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    val tilesState = remember { mutableStateListOf(*tiles.toTypedArray()) }
+
+// This is an example of a list of dismissible items, similar to what you would see in an
+// email app. Swiping left reveals a 'delete' icon and swiping right reveals a 'done' icon.
+// The background will start as grey, but once the dismiss threshold is reached, the colour
+// will animate to red if you're swiping left or green if you're swiping right. When you let
+// go, the item will animate out of the way if you're swiping left (like deleting an email) or
+// back to its default position if you're swiping right (like marking an email as read/unread).
+    LazyColumn {
+        items(
+            count = tiles.size,
+            key = { index -> tiles[index] },
+        ) { item: Int ->
+            var unread by remember { mutableStateOf(false) }
+            val dismissState =
+                rememberDismissState(
+                    confirmStateChange = {
+                        if (it == DismissValue.DismissedToEnd) unread = !unread
+                        it != DismissValue.DismissedToEnd
+                    },
+                )
+//            val dismissState = rememberDismissState(
+//                confirmStateChange = {
+//                    if (it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart) {
+//                        tilesState.remove(tiles[item])
+//                        scope.launch {
+//                            val result = snackbarHostState.showSnackbar(
+//                                message = "Item deleted",
+//                                actionLabel = "Undo"
+//                            )
+//                            if (result == SnackbarResult.ActionPerformed) {
+//                                tilesState.add(tiles[item])
+//                                onUndo(tiles[item])
+//                            }
+//                        }
+//                    }
+//                    true
+//                }
+//            )
+            SwipeToDismiss(
+                state = dismissState,
+                modifier = Modifier.padding(vertical = 4.dp),
+                directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
+                background = {
+                    val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
+                    val color by
+                        animateColorAsState(
+                            when (dismissState.targetValue) {
+                                DismissValue.Default -> Color.LightGray
+                                DismissValue.DismissedToEnd -> Color.Green
+                                DismissValue.DismissedToStart -> Color.Red
+                            },
+                            label = "background-color",
+                        )
+                    val alignment =
+                        when (direction) {
+                            DismissDirection.StartToEnd -> Alignment.CenterStart
+                            DismissDirection.EndToStart -> Alignment.CenterEnd
+                        }
+                    val icon =
+                        when (direction) {
+                            DismissDirection.StartToEnd -> Icons.Default.Done
+                            DismissDirection.EndToStart -> Icons.Default.Delete
+                        }
+                    val scale by
+                        animateFloatAsState(
+                            if (dismissState.targetValue == DismissValue.Default) 0.75f else 1f,
+                            label = "icon-scale",
+                        )
+
+                    Box(
+                        Modifier.fillMaxSize().background(color).padding(horizontal = 20.dp),
+                        contentAlignment = alignment,
+                    ) {
+                        Icon(
+                            icon,
+                            contentDescription = "Localized description",
+                            modifier = Modifier.scale(scale),
+                        )
+                    }
+                },
+                dismissContent = {
+                    val cardElevation: Dp =
+                        animateDpAsState(
+                            if (dismissState.dismissDirection != null) 8.dp else 4.dp,
+                            label = "card-elevation",
+                        ).value
+                    AlertTile(data = tiles[item], cardElevation, modifier = Modifier.fillMaxWidth())
+                },
+            )
         }
     }
 }
@@ -204,12 +311,13 @@ fun AlertTileGrid(tiles: List<AlertTileData>) {
 @Composable
 fun AlertTile(
     data: AlertTileData,
+    cardElevation: Dp,
     modifier: Modifier = Modifier,
 ) {
     Card(
         modifier = modifier.padding(8.dp),
         shape = RoundedCornerShape(12.dp),
-        elevation = CardDefaults.cardElevation(4.dp),
+        elevation = CardDefaults.cardElevation(cardElevation),
     ) {
         Column(
             modifier =
