3b72dd5372c8aa681d7f0dfe50f0424e00f721dc
Hossain Khan <hello@hossain.dev>
2025-01-06 19:11:57 -0500

    [FIXED] DB mismatch issue

--- app/src/main/java/dev/hossain/weatheralert/db/City.kt
+++ app/src/main/java/dev/hossain/weatheralert/db/City.kt
@@ -1,9 +1,36 @@
 package dev.hossain.weatheralert.db
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "cities")
+/**
+ *
+ * ```sql
+ * CREATE TABLE "cities" (
+ * 	"city"	TEXT NOT NULL,
+ * 	"city_ascii"	TEXT NOT NULL,
+ * 	"lat"	REAL NOT NULL,
+ * 	"lng"	REAL NOT NULL,
+ * 	"country"	TEXT NOT NULL,
+ * 	"iso2"	TEXT NOT NULL,
+ * 	"iso3"	TEXT NOT NULL,
+ * 	"admin_name"	TEXT,
+ * 	"capital"	TEXT,
+ * 	"population"	INTEGER,
+ * 	"id"	INTEGER NOT NULL,
+ * 	PRIMARY KEY("id")
+ * )
+ * ```
+ */
+@Entity(
+    tableName = "cities",
+    indices = [
+        Index(name = "idx_country", value = ["country"], unique = false),
+        Index(name = "idx_city_ascii", value = ["city_ascii"], unique = false),
+        Index(name = "idx_city", value = ["city"], unique = false),
+    ],
+)
 data class City(
     val city: String,
     val city_ascii: String,
@@ -12,7 +39,7 @@ data class City(
     val country: String,
     val iso2: String,
     val iso3: String,
-    val admin_name: String,
+    val admin_name: String?,
     val capital: String?,
     val population: Int?,
     @PrimaryKey val id: Int,
