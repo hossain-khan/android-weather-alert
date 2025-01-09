f24b5028db272855c61450aa20917ee10c6659e8
Hossain Khan <hello@hossain.dev>
2025-01-09 09:35:21 -0500

    [ADDED] Error code with docs from https://openweathermap.org/faq#error401

--- app/src/main/java/dev/hossain/weatheralert/api/WeatherApi.kt
+++ app/src/main/java/dev/hossain/weatheralert/api/WeatherApi.kt
@@ -20,7 +20,41 @@ interface WeatherApi {
     companion object {
         // Units of measurement - `standard`, `metric`, and `imperial` units are available.
         // However, `metric` as baseline and conversion will be used later if needed.
-        const val UNIT_METRIC = "metric"
+        internal const val UNIT_METRIC = "metric"
+
+        /**
+         * You can get the error 401 in the following cases:
+         *
+         * - You did not specify your API key in API request.
+         * - Your API key is not activated yet. Within the next couple of hours, it will be activated and ready to use.
+         * - You are using wrong API key in API request. Please, check your right API key in personal account.
+         * - You are using a Free subscription and try requesting data available in other subscriptions .
+         *   For example, 16 days/daily forecast API, any historical weather data, Weather maps 2.0, etc).
+         *   Please, check your subscription in your personal account.
+         */
+        internal const val ERROR_HTTP_UNAUTHORIZED = 401
+
+        /**
+         * You can get this error when you specified the wrong city name, ZIP-code or city ID.
+         * For your reference, this list contains City name, City ID, Geographical coordinates
+         * of the city (lon, lat), Zoom, etc.
+         *
+         * You can also get the error 404 if the format of your API request is incorrect.
+         * In this case, please review it and check for any mistakes. To see examples of
+         * correct API requests, please visit the Documentation of a specific API and read
+         * the examples of API calls there.
+         */
+        internal const val ERROR_HTTP_NOT_FOUND = 404
+
+        /**
+         * You can receive this error in the following cases:
+         *
+         * - If you have a Free plan of Professional subscriptions and make more than 60 API calls
+         *   per minute (surpassing the limit of your subscription). To avoid this situation,
+         *   please consider upgrading to a subscription plan that meets your needs or reduce the
+         *   number of API calls in accordance with the limits.
+         */
+        internal const val ERROR_HTTP_TOO_MANY_REQUESTS = 429
     }
 
     @GET("data/3.0/onecall")
