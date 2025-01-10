1231f22f00289e227659d0fd51235d5644c00dda
Hossain Khan <hello@hossain.dev>
2025-01-09 22:37:18 -0500

    [ADDED] Logic to load user provided API key for future calls

--- app/src/main/java/dev/hossain/weatheralert/data/PreferencesManager.kt
+++ app/src/main/java/dev/hossain/weatheralert/data/PreferencesManager.kt
@@ -6,7 +6,9 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import dev.hossain.weatheralert.di.ApplicationContext
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 /**
@@ -27,9 +29,27 @@ class PreferencesManager
                     preferences[UserPreferences.userApiKey]
                 }
 
+        /**
+         * Retrieves the saved API key from the user preferences in synchronous manner.
+         */
+        val savedApiKey: String?
+            get() =
+                runBlocking {
+                    dataStore.data
+                        .map { preferences: Preferences ->
+                            preferences[UserPreferences.userApiKey]
+                        }.firstOrNull()
+                }
+
         suspend fun saveUserApiKey(apiKey: String) {
             dataStore.edit { preferences: MutablePreferences ->
                 preferences[UserPreferences.userApiKey] = apiKey
+            }
+        }
+
+        suspend fun clearUserApiKey() {
+            dataStore.edit { preferences: MutablePreferences ->
+                preferences.remove(UserPreferences.userApiKey)
             }
         }
     }
