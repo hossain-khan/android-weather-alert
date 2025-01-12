66bebec14591c997d4df527a1648db5581a7c8d0
Hossain Khan <hello@hossain.dev>
2025-01-12 13:09:34 -0500

    [UPDATE] App version bump for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "dev.hossain.weatheralert"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         // Read bundled API key from local.properties
         val openWeatherApiKey: String =
