6eb759d6faa69544eb4540b7cb2214ee88b21e14
Hossain Khan <hello@hossain.dev>
2025-01-24 18:08:58 -0500

    [ADDED] Learn more bottom sheet as education piece
    Also added the disclaimer

--- app/src/main/java/dev/hossain/weatheralert/ui/alertslist/EmptyAlertView.kt
+++ app/src/main/java/dev/hossain/weatheralert/ui/alertslist/EmptyAlertView.kt
@@ -5,14 +5,24 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -22,11 +32,17 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.hossain.weatheralert.R
+import dev.hossain.weatheralert.ui.about.LearnMoreAboutAlerts
 import dev.hossain.weatheralert.ui.theme.WeatherAlertAppTheme
 import dev.hossain.weatheralert.ui.theme.dimensions
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EmptyAlertState(modifier: Modifier = Modifier) {
+    // https://developer.android.com/develop/ui/compose/components/bottom-sheets
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    var showBottomSheet by remember { mutableStateOf(false) }
+
     Column(
         modifier =
             modifier
@@ -49,6 +65,11 @@ fun EmptyAlertState(modifier: Modifier = Modifier) {
             text = "No custom weather alerts configured.",
             style = MaterialTheme.typography.bodySmall,
         )
+        Spacer(modifier = Modifier.height(2.dp))
+        TextButton(onClick = {
+            showBottomSheet = true
+        }) { Text("Learn more") }
+
         Column(modifier = Modifier.padding(vertical = 80.dp)) {
             Text(
                 text = "Powered by,",
@@ -95,6 +116,19 @@ fun EmptyAlertState(modifier: Modifier = Modifier) {
                         .alpha(0.9f),
                 contentDescription = "Tomorrow.io Logo",
             )*/
+        }
+    }
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                showBottomSheet = false
+            },
+            sheetState = sheetState,
+        ) {
+            LearnMoreAboutAlerts {
+                showBottomSheet = false
+            }
         }
     }
 }
