4fcc4bca5303aa814d32f1d9b71c8c9a1beeee00
Hossain Khan <hello@hossain.dev>
2025-02-02 06:28:07 -0500

    [FIXED] Release build issue. Now release variant can be used.

--- data-model/build.gradle.kts
+++ data-model/build.gradle.kts
@@ -24,6 +24,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
